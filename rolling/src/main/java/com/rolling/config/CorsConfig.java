dbc3c27458edccf2fb53c4ecd2bb92f6f794529c
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2025-04-19 04:02:21 +0900

    feat: CORS 설정을 위한 CorsConfig 클래스 추가

---
+++ rolling/src/main/java/com/rolling/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.rolling.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("http://localhost:3000");
+
+        // 모든 헤더 허용
+        config.addAllowedHeader("*");
+
+        // 모든 HTTP 메서드 허용
+        config.addAllowedMethod("*");
+
+        // 인증 정보 허용
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
