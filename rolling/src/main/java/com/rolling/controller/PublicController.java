1759813ff68d2624243f5668471205e5796db810
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2025-04-19 04:02:45 +0900

    feat: PublicController 클래스 추가 및 배경 이미지, 프로필 이미지 API 엔드포인트 구현

---
+++ rolling/src/main/java/com/rolling/controller/PublicController.java
@@ -0,0 +1,41 @@
+package com.rolling.controller;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rolling.model.ServiceResult;
+import com.rolling.service.PublicService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class PublicController {
+
+    private final PublicService publicService;
+
+    @GetMapping("/background-images/")
+    public ResponseEntity<Object> getBackgroundImage() {
+
+        ServiceResult serviceResult = publicService.getBackgroundImage();
+
+        if (!serviceResult.isSuccess()) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(serviceResult.getMessage());
+        }
+
+        return ResponseEntity.ok(serviceResult.getData());
+    }
+
+    @GetMapping("/profile-images/")
+    public ResponseEntity<Object> getProfileImage() {
+        ServiceResult serviceResult = publicService.getProfileImage();
+
+        return ResponseEntity.ok(serviceResult.getData());
+    }
+}
