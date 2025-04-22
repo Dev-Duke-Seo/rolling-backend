f3f7d10eb604c75dbf5be9e25f8e36928f1e2474
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2025-04-22 18:27:57 +0900

    feat: ColorTypeConverter 클래스 추가하여 ColorType 열거형과 데이터베이스 간 변환 기능 구현 - ColorType 열거형을 데이터베이스 문자열로 변환하는 convertToDatabaseColumn 메서드 추가 - 데이터베이스 문자열을 ColorType 열거형으로 변환하는 convertToEntityAttribute 메서드 추가 - 변환 중 예외 처리 로직 포함

---
+++ rolling/src/main/java/com/rolling/model/enums/ColorTypeConverter.java
@@ -0,0 +1,31 @@
+package com.rolling.model.enums;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class ColorTypeConverter implements AttributeConverter<ColorType, String> {
+
+    @Override
+    public String convertToDatabaseColumn(ColorType attribute) {
+        if (attribute == null) {
+            return null;
+        }
+        return attribute.getValue(); // 예: "#800080" 저장
+    }
+
+    @Override
+    public ColorType convertToEntityAttribute(String dbData) {
+        if (dbData == null) {
+            return null;
+        }
+        //
+        for (ColorType color : ColorType.values()) {
+            if (color.getValue().equals(dbData)) {
+                return color;
+            }
+        }
+
+        throw new IllegalArgumentException("Unknown database value: " + dbData);
+    }
+}
